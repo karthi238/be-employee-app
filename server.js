a4929761585ea5a02fdab3fd049b693c695c45e3
Karthick <karthick.anbu-c@fintso.com>
2021-03-29 22:16:49 +0530

    initial code

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+// create express app
+const app = express();
+
+
+// Setup server port
+const port = process.env.PORT || 5000;
+
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+
+// define a root route
+app.get('/', (req, res) => {
+  res.send("Welcome to Employee Management");
+});
+
+
+// Require employee routes
+const employeeRoutes = require('./src/routes/employee.routes');
+
+
+// using as middleware
+app.use(cors());
+app.use('/api/v1/employees', employeeRoutes)
+
+
+// listen for requests
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
